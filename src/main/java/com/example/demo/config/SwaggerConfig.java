237d8ad63153c3a70935cc956b68387b1274df24
bala-amypo <bala@amypo.in>
2025-12-25 08:10:09 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,9 +13,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Smart Grid Load Shedding Controller")
-                        .version("1.0")
-                        .description("Swagger API Preview"));
+                .servers(List.of(
+                        new Server().url("http://localhost:9001")
+                ));
     }
 }
