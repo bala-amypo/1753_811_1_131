a3ef37cc7dcd3c1fa382060543e9c4962639bb2f
bala-amypo <bala@amypo.in>
2025-12-25 10:04:07 +0000

    User commit

--- src/main/java/com/example/demo/repository/ZoneRestorationRecordRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRestorationRecordRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ZoneRestorationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
-@Repository
-public interface ZoneRestorationRepository
+public interface ZoneRestorationRecordRepository
         extends JpaRepository<ZoneRestorationRecord, Long> {
+
+    List<ZoneRestorationRecord> findByZoneId(Long zoneId);
 }
